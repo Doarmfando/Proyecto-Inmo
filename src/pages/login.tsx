7148d1cad019eecf834ad3a6738ed2f0b7f7d261
DioBrando0203 <brandoarmas@hotmail.com>
2025-07-06 22:20:35 -0500

    Mejora Visual Login V1

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,25 +3,40 @@ import PasswordInput from "../components/PasswordInput";
 import RememberMe from "../components/RememberMe";
 import { FaUser } from "react-icons/fa";
 import styles from "../styles/login.module.css";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-1.png";
+import miniLogo from "../assets/mini-logo.png";
 
 export default function Login() {
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <div className={styles.header}>
-            <img src={logo} alt="Logo inmobiliaria" className={styles.logo} />
-          <h1 className={styles.title}>Inicia Sesión</h1>
-        </div>
+    <div className={styles.wrapper}>
+      {/* Columna azul izquierda */}
+      <div className={styles.leftPanel}>
+        <img src={logo} alt="Logo" className={styles.sideLogo} />
+      </div>
 
-        <form className={styles.form}>
-          <InputWithIcon icon={<FaUser />} placeholder="Usuario o correo" />
-          <PasswordInput />
-          <RememberMe />
-          <button type="submit" className={styles.button}>
-            Iniciar Sesión
-          </button>
-        </form>
+      {/* Área principal del login */}
+      <div className={styles.mainContent}>
+        <div className={styles.card}>
+          <div className={styles.header}>
+  
+            <h1 className={styles.title}>Inicia Sesión</h1>
+          </div>
+          <form className={styles.form}>
+            <InputWithIcon icon={<FaUser />} placeholder="Usuario o correo" />
+            <PasswordInput />
+            <RememberMe />
+            <button type="submit" className={styles.button}>
+              Iniciar Sesión
+            </button>
+          </form>
+        </div>
+      </div>
+
+      {/* Franja amarilla inferior */}
+      <div className={styles.bottomBanner}>
+        <img src={miniLogo} alt="Mini logo" className={styles.miniLogo} />
+
+        <span className={styles.powered}>Powered by</span>
       </div>
     </div>
   );
